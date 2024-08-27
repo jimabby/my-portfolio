d6aa7e14eb59f8bc22008ad76f0f18f6c3cb53f0
jim <wksunshine@gmail.com>
2024-08-27 21:42:21 +1000

    Scroll up button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
+import ScrollUp from './components/scrollup/ScrollUp';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 import Testimonials from './components/Testimonials/Testimonials';
@@ -21,9 +22,11 @@ function App() {
         <Services />
         <Qualification />
         <Testimonials />
-        <Contact />
-        <Footer />
+        <Contact />  
       </main>
+      
+      <Footer />
+      <ScrollUp />
     </>
   );
 }
